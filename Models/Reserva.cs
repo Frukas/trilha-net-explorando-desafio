7a788ec4c8a66e01ebf6980f54001e6f163b06c2
Felipe Garcia <garushiafm@gmail.com>
2025-06-08 10:37:42 -0300

    resolução da tarefa

--- Models/Reserva.cs
+++ Models/Reserva.cs
@@ -15,9 +15,14 @@ namespace DesafioProjetoHospedagem.Models
 
         public void CadastrarHospedes(List<Pessoa> hospedes)
         {
+        
+            if(this.Suite == null){
+                 new Exception("Suites não cadastrado");
+            }
             // TODO: Verificar se a capacidade é maior ou igual ao número de hóspedes sendo recebido
             // *IMPLEMENTE AQUI*
-            if (true)
+
+            if (hospedes.Count <= this.Suite.Capacidade)
             {
                 Hospedes = hospedes;
             }
@@ -25,6 +30,7 @@ namespace DesafioProjetoHospedagem.Models
             {
                 // TODO: Retornar uma exception caso a capacidade seja menor que o número de hóspedes recebido
                 // *IMPLEMENTE AQUI*
+               throw new Exception("A quantidade de hóspedes excede a capacidade da suite.");
             }
         }
 
@@ -35,23 +41,26 @@ namespace DesafioProjetoHospedagem.Models
 
         public int ObterQuantidadeHospedes()
         {
-            // TODO: Retorna a quantidade de hóspedes (propriedade Hospedes)
-            // *IMPLEMENTE AQUI*
-            return 0;
+            if(this.Hospedes == null){
+                throw new Exception("Hóspedes não cadastrados");
+            }
+            
+            return this.Hospedes.Count();
         }
 
         public decimal CalcularValorDiaria()
         {
+            decimal dezPorCentroDesconto = 0.90M;
             // TODO: Retorna o valor da diária
             // Cálculo: DiasReservados X Suite.ValorDiaria
             // *IMPLEMENTE AQUI*
-            decimal valor = 0;
+            decimal valor = this.Suite.ValorDiaria * this.DiasReservados;
 
             // Regra: Caso os dias reservados forem maior ou igual a 10, conceder um desconto de 10%
             // *IMPLEMENTE AQUI*
-            if (true)
+            if (this.DiasReservados >= 10)
             {
-                valor = 0;
+                valor *= dezPorCentroDesconto;
             }
 
             return valor;
